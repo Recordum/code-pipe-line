ac45fdab238e5fc87098987bf8d7042d45964158
Recordum <mingyu.oh15b@gmail.com>
2023-10-26 14:57:34 +0900

    Modify: login 다시 추가

--- scripts/pull_and_run_container.sh
+++ scripts/pull_and_run_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/home/ubuntu/login-docker.sh
+
+$(aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 368081054787.dkr.ecr.ap-northeast-2.amazonaws.com)
 
 docker pull 368081054787.dkr.ecr.ap-northeast-2.amazonaws.com/sample-ecr:latest
 
