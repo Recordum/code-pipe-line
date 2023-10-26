1ef9cadd730c13f8a13beb1167b5c5e307c72e8c
Recordum <mingyu.oh15b@gmail.com>
2023-10-26 11:04:28 +0900

    modify: appspec 수정

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 if [ "$(docker ps -q -f name=server-sample)" ]; then
+    echo "Stopping server-sample container..."
     docker stop server-sample
+else
+    echo "server-sample container is not running."
 fi
